a0b36f919f6e7ac80a90b74db558e3559fec64e4
unsky <2081264@qq.com>
2017-08-15 18:32:03 +0800

    focal loss

--- focal_loss.py
+++ focal_loss.py
@@ -1,11 +1,11 @@
 # --------------------------------------------------------
 # Focal loss
 # Licensed under The Apache-2.0 License [see LICENSE for details]
-# Written by chenshuai
+# Written by chen shuai
 # --------------------------------------------------------
 
 """
-focal loss with softmax
+Focal loss 
 """
 
 import mxnet as mx
@@ -19,38 +19,38 @@ class FocalLossOperator(mx.operator.CustomOp):
 
     def forward(self, is_train, req, in_data, out_data, aux):
 
-        cls_score    = in_data[0]
-        bbox_pred    = in_data[1]
-        labels       = in_data[2].asnumpy()
-        bbox_targets = in_data[3]
-        bbox_weights = in_data[4]
+        cls_score = in_data[0]
+        self._cls_score = cls_score
+        labels = in_data[1].asnumpy()
+        self._labels = labels
 
-        per_roi_loss_cls = mx.nd.SoftmaxActivation(cls_score) + 1e-14
-        per_roi_loss_cls = per_roi_loss_cls.asnumpy()
-        per_roi_loss_cls = per_roi_loss_cls[np.arange(per_roi_loss_cls.shape[0], dtype='int'), labels.astype('int')]
-        per_roi_loss_cls = -1 * np.log(per_roi_loss_cls)
-        per_roi_loss_cls = np.reshape(per_roi_loss_cls, newshape=(-1,))
+        pro_ = mx.nd.SoftmaxActivation(cls_score) + 1e-14
+        pro_ = pro_.asnumpy()
+        # restore pt for backward
+        self._pt = pro_[np.arange(pro_.shape[0],dtype = 'int'), labels.astype('int')]
 
-        per_roi_loss_bbox = bbox_weights * mx.nd.smooth_l1((bbox_pred - bbox_targets), scalar=1.0)
-        per_roi_loss_bbox = mx.nd.sum(per_roi_loss_bbox, axis=1).asnumpy()
+        ### note!!!!!!!!!!!!!!!!
+        # focal loss value is not used in this place we should forward the cls_pro in this layer, the focal vale should be calculated in metric.py
+        # the method is in readme
+        #  focal loss (batch_size,num_class)
+        loss_ = -1 * np.power(1 - pro_, self._gamma) * np.log(pro_)
 
-        top_k_per_roi_loss = np.argsort(per_roi_loss_cls + per_roi_loss_bbox)
-        labels_ohem = labels
-        labels_ohem[top_k_per_roi_loss[::-1][self._roi_per_img:]] = -1
-        bbox_weights_ohem = bbox_weights.asnumpy()
-        bbox_weights_ohem[top_k_per_roi_loss[::-1][self._roi_per_img:]] = 0
-
-        labels_ohem = mx.nd.array(labels_ohem)
-        bbox_weights_ohem = mx.nd.array(bbox_weights_ohem)
-
-        for ind, val in enumerate([labels_ohem, bbox_weights_ohem]):
-            self.assign(out_data[ind], req[ind], val)
-
-
+        self.assign(out_data[0],req[0],mx.nd.array(pro_))
     def backward(self, req, out_grad, in_data, out_data, in_grad, aux):
-        for i in range(len(in_grad)):
-            self.assign(in_grad[i], req[i], 0)
-
+        cls_score = self._cls_score
+        labels = self._labels
+        pro_ = mx.nd.SoftmaxActivation(cls_score) + 1e-14
+        pro_ = pro_.asnumpy()
+        #i!=j
+        pt = self._pt
+        dx = np.power(1 - pro_, self._gamma - 1)
+        pt = pt.reshape(len(pt),1)
+        dx = np.power(1 - pt, self._gamma - 1) *(self._gamma * (-1 * pt * pro_) * np.log(pro_) + pt * (1 - pro_))
+        ####i==j 
+        #reload pt
+        pt = self._pt 
+        dx[np.arange(pro_.shape[0],dtype = 'int'), labels.astype('int')] = np.power(1 - pt, self._gamma) * (self._gamma * pt * np.log(pt) + pt -1) 
+        self.assign(in_grad[0], req[0], mx.nd.array(dx))
 
 @mx.operator.register('FocalLoss')
 class FocalLossProp(mx.operator.CustomOpProp):
@@ -59,10 +59,8 @@ class FocalLossProp(mx.operator.CustomOpProp):
         self._num_classes = int(num_classes)
         self._gamma = float(gamma)
 
-
-
     def list_arguments(self):
-        return ['data', 'label']
+        return ['data', 'labels']
 
     def list_outputs(self):
         return ['focal_loss']
@@ -70,11 +68,11 @@ class FocalLossProp(mx.operator.CustomOpProp):
     def infer_shape(self, in_shape):
         data_shape = in_shape[0]
         labels_shape = in_shape[1]
-        return in_shape, \
-               [data_shape, labels_shape]
+        out_shape = data_shape
+        return  [data_shape, labels_shape],[out_shape]
 
     def create_operator(self, ctx, shapes, dtypes):
-        return FocalLossOperator(self._num_classes, self.gama)
+        return FocalLossOperator(self._num_classes, self._gamma)
 
     def declare_backward_dependency(self, out_grad, in_data, out_data):
         return []
